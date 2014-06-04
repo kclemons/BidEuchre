f5be79b4e93d4b484c53993f3eb83ff6f68a8b01
kclemons <cl3m0ns@gmail.com>
2014-06-04 19:49:11 -0400

    big time refactor of bid class to make it awesome. not done but pushing now while it's slightly stable

--- Src/BidClass.js
+++ Src/BidClass.js
@@ -2,12 +2,6 @@
 //1. set up the game so the user is always index 0. DONE
 //2. assign the 4 pc players names, x and y cords, etc are stored in their perspective array.
 //3. per josh: deal out 1 cards to each player until they're gone. ((possibly in a randomized order for the player array)
-
-var chooseRandomDealerFunc = function(maxValue){
-		var randNumber = Math.floor(Math.random() * maxValue) + 1;
-		return randNumber;
-};
-
 var audioEngine = cc.AudioEngine.getInstance();
 
 var BidClass = cc.LayerColor.extend({
@@ -18,7 +12,9 @@ var BidClass = cc.LayerColor.extend({
 	BidOptions: [0, 4, 5, 6, 7, 8],
 	HighestCurrentBid: 0,
 	IndexOfBidWinner: 0,
-	Players:[],
+	Players:[], //contains the following::::
+	 		//{ Bid(num), isDealer(bool), Cards(arr), PlayerId(num), OrderNumber(num), isHuman: true
+			//	isDealer(bool), TeammemberIds(arr)}
 	CurrentCardIndexForDealing: 0, //dumb thing used for dealering 2 /3 / 2 to players...needs refactor, not an important var
 	CurrentDealerIndex: 0,
 	CurrentPlayersTurnIndex: 0,
@@ -54,7 +50,7 @@ var BidClass = cc.LayerColor.extend({
 		this.createOpponentNamesPositions(); //creates labels for the AI player names
 
 		this.dealCards();
-		this.displayHumanPlayersCards();//future Kyle: here you can see randy's infulence in the naming conventions of your code
+		this.displayHumanPlayersCards();
 
 		this.displayDealerChip();
 		this.displayCurrentPlayersTurnIndicator();
@@ -77,23 +73,7 @@ var BidClass = cc.LayerColor.extend({
 		
 	},
 	setupBidLogic: function () {
-		cc.log(this.initialCall + ",  " + this.StartVal + ",  " +  this.EndVal);
-		for(var i = this.StartVal; i < this.EndVal; i++){
-			var currentPlayer = this.Players[i];
-			if(!this.initialCall && i === this.OriginalTurnIndex){
-				cc.log('bidding complete!');
-			}else if(currentPlayer.isHuman){
-				this.displayHumanPlayersBidOptions();
-				cc.log('users is bidding so we break out and wait for him to finish');
-				return;
-			}else{
-				cc.log('else');
-				this.setupAiBidLogic();
-			}
-			if(i === 4 && this.OriginalTurnIndex !== 0){
-				i = 0;
-			}
-		}
+
 	},
 	setupAiBidLogic: function (){
 		cc.log("called");
@@ -157,118 +137,19 @@ var BidClass = cc.LayerColor.extend({
 		var cache = cc.SpriteFrameCache.getInstance();
         cache.addSpriteFrames(spritePlist, cardSprite);
 
-        var tempArr = [];
-
-        var AceClubs = { CardName: "Ace", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Clubs.png"), CardId: 0};
-        var KingClubs = { CardName: "King", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Clubs.png"), CardId: 0};
-        var QueenClubs = { CardName: "Queen", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Clubs.png"), CardId: 0};
-        var JackClubs = { CardName: "Jack", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Clubs.png"), CardId: 0};
-        var TenClubs = { CardName: "Ten", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Clubs.png"), CardId: 0};
-        var NineClubs = { CardName: "Nine", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Clubs.png"), CardId: 0};
-
-        var AceClubs2 = { CardName: "Ace", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Clubs.png"), CardId: 0};
-        var KingClubs2 = { CardName: "King", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Clubs.png"), CardId: 0};
-        var QueenClubs2 = { CardName: "Queen", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Clubs.png"), CardId: 0};
-        var JackClubs2 = { CardName: "Jack", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Clubs.png"), CardId: 0};
-        var TenClubs2 = { CardName: "Ten", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Clubs.png"), CardId: 0};
-        var NineClubs2 = { CardName: "Nine", CardSuit: "Clubs", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Clubs.png"), CardId: 0};
-
-        var AceSpades = { CardName: "Ace", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Spades.png"), CardId: 0};
-        var KingSpades = { CardName: "King", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Spades.png"), CardId: 0};
-        var QueenSpades = { CardName: "Queen", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Spades.png"), CardId: 0};
-        var JackSpades = { CardName: "Jack", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Spades.png"), CardId: 0};
-        var TenSpades = { CardName: "Ten", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Spades.png"), CardId: 0};
-        var NineSpades = { CardName: "Nine", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Spades.png"), CardId: 0};
-
-        var AceSpades2 = { CardName: "Ace", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Spades.png"), CardId: 0};
-        var KingSpades2 = { CardName: "King", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Spades.png"), CardId: 0};
-        var QueenSpades2 = { CardName: "Queen", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Spades.png"), CardId: 0};
-        var JackSpades2 = { CardName: "Jack", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Spades.png"), CardId: 0};
-        var TenSpades2 = { CardName: "Ten", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Spades.png"), CardId: 0};
-        var NineSpades2 = { CardName: "Nine", CardSuit: "Spades", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Spades.png"), CardId: 0};
-
-        var AceHearts = { CardName: "Ace", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Hearts.png"), CardId: 0};
-        var KingHearts = { CardName: "King", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Hearts.png"), CardId: 0};
-        var QueenHearts = { CardName: "Queen", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Hearts.png"), CardId: 0};
-        var JackHearts = { CardName: "Jack", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Hearts.png"), CardId: 0};
-        var TenHearts = { CardName: "Ten", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Hearts.png"), CardId: 0};
-        var NineHearts = { CardName: "Nine", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Hearts.png"), CardId: 0};
-
-        var AceHearts2 = { CardName: "Ace", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Hearts.png"), CardId: 0};
-        var KingHearts2 = { CardName: "King", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Hearts.png"), CardId: 0};
-        var QueenHearts2 = { CardName: "Queen", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Hearts.png"), CardId: 0};
-        var JackHearts2 = { CardName: "Jack", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Hearts.png"), CardId: 0};
-        var TenHearts2 = { CardName: "Ten", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Hearts.png"), CardId: 0};
-        var NineHearts2 = { CardName: "Nine", CardSuit: "Hearts", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Hearts.png"), CardId: 0};
-
-        var AceDiamonds = { CardName: "Ace", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Diamonds.png"), CardId: 0};
-        var KingDiamonds = { CardName: "King", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Diamonds.png"), CardId: 0};
-        var QueenDiamonds = { CardName: "Queen", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Diamonds.png"), CardId: 0};
-        var JackDiamonds = { CardName: "Jack", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Diamonds.png"), CardId: 0};
-        var TenDiamonds = { CardName: "Ten", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Diamonds.png"), CardId: 0};
-        var NineDiamonds = { CardName: "Nine", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Diamonds.png"), CardId: 0};
-
-        var AceDiamonds2 = { CardName: "Ace", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ace_Diamonds.png"), CardId: 0};
-        var KingDiamonds2 = { CardName: "King", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("King_Diamonds.png"), CardId: 0};
-        var QueenDiamonds2 = { CardName: "Queen", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Queen_Diamonds.png"), CardId: 0};
-        var JackDiamonds2 = { CardName: "Jack", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Jack_Diamonds.png"), CardId: 0};
-        var TenDiamonds2 = { CardName: "Ten", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Ten_Diamonds.png"), CardId: 0};
-        var NineDiamonds2 = { CardName: "Nine", CardSuit: "Diamonds", SpriteRef: cc.Sprite.createWithSpriteFrameName("Nine_Diamonds.png"), CardId: 0};
+        var temporaryCardArr = createCards(cache);
         
-        tempArr.push(AceClubs, KingClubs, QueenClubs, JackClubs, TenClubs, 
-        		AceSpades, KingSpades, QueenSpades, JackSpades, TenSpades, 
-        		AceHearts, KingHearts, QueenHearts, JackHearts, TenHearts, 
-        		AceDiamonds, KingDiamonds, QueenDiamonds, JackDiamonds, TenDiamonds,
-        		AceClubs2, KingClubs2, QueenClubs2, JackClubs2, TenClubs2, 
-        		AceSpades2, KingSpades2, QueenSpades2, JackSpades2, TenSpades2, 
-        		AceHearts2, KingHearts2, QueenHearts2, JackHearts2, TenHearts2, 
-        		AceDiamonds2, KingDiamonds2, QueenDiamonds2, JackDiamonds2, TenDiamonds2);
+        this.Cards = shuffleArray(temporaryCardArr);
 
-        this.Cards = shuffleArrayHelperFunc(tempArr);
-
-        //code to log the cars and make sure they got set
+        //code to set the ID's for the Cards
         for(var i = 0; i < this.Cards.length; i++){
         	var currentCard = this.Cards[i];
         	currentCard.CardId = i + 1;
         }
 	},
 	createPlayers: function () {
-		var tempPlayersArr = [];
-		var humanPlayer = { OrderNumber: 0,
-        						Cards: [],
-        						isHuman: true,
-        						PlayerId: 0,
-        						isDealer: false,
-        						TeammemberIds: []
-    						 };
-
-		tempPlayersArr.push(humanPlayer,
-    	 		{ OrderNumber: 0,
-					Cards: [],
-					isHuman: false,
-					PlayerId: 0,
-					isDealer: false,
-					TeammemberIds: []
-				 }, { OrderNumber: 0,
-					Cards: [],
-					isHuman: false,
-					PlayerId: 0,
-					isDealer: false,
-					TeammemberIds: []
-				 }, { OrderNumber: 0,
-					Cards: [],
-					isHuman: false,
-					PlayerId: 0,
-					isDealer: false,
-					TeammemberIds: []
-				 }, { OrderNumber: 0,
-					Cards: [],
-					isHuman: false,
-					PlayerId: 0,
-					isDealer: false,
-					TeammemberIds: []
-				 });
-
+		
+		var tempPlayersArr = createPlayers();
 		this.Players = tempPlayersArr;
 
 		this.CurrentDealerIndex = chooseRandomDealerFunc(5) - 1;
@@ -288,6 +169,7 @@ var BidClass = cc.LayerColor.extend({
 
   		for(var i = 0; i < this.Players.length; i++){
         	this.Players[i].PlayerId = i + 1;
+        	cc.log("playerId: " + this.Players[i].PlayerId + "/////   isHuman: " + this.Players[i].isHuman + "////  index:" + i );
         }
 	},
 	createHumanPlayerCardSpots: function() {
@@ -309,118 +191,57 @@ var BidClass = cc.LayerColor.extend({
 		playerMarker.setPosition(new cc.Point(markerX, markerY));
 		this.addChild(playerMarker);
 	},
+	createAiTopSpots: function (spotsToCreate){
+		var spaceBetween = winSize.width/spotsToCreate;
+		var padding = spaceBetween/2;
+		var y = winSize.height;
+		var x = padding;
+		for(var i = 0; i < spotsToCreate; i++){
+			var msg = "Dr. Doom";
+			if(i === 1){
+				msg = "Lord Business";
+			}else if(i === 2){
+				msg = "Guy Richie";
+			}
+			var aiSpot = createPlayerMarker(x, y);
+			this.addChild(aiSpot);
+
+			var playerLabel = creatAiLabel(msg, x, y-30)
+			this.addChild(playerLabel);
+
+			x = x + spaceBetween;
+		}
+	},
 	createOpponentNamesPositions: function () {
 		var x = 0;
 		var y = winSize.height/1.5;
-		var playerArr = this.Players;
-		var aiIndex = 0;
-		this.HumanPlayerIndex = 0;
-		if(this.HumanPlayerIndex === playerArr.length -1){
-			aiIndex = 0;
-		}else{
-			aiIndex = this.HumanPlayerIndex + 1;
-		}
-		var leftOfSpot = cc.Sprite.create(aiSpotMarker);
-		var leftlabel = cc.LabelTTF.create("Darth Vader", "Arial", 14);
-        
-		leftOfSpot.setPosition(new cc.Point(x, y));
-		this.addChild(leftOfSpot);
 
-		leftlabel.setColor(cc.c3b(0,0,0));
-        leftlabel.setPosition(new cc.Point(x+40, y));
-        this.addChild(leftlabel);
+		//ai player to the left of human
+		var leftSpot = createPlayerMarker(x, y)
+		this.addChild(leftSpot);
+		var leftAiLabel = creatAiLabel("Darth Vader", x+40, y);
+		this.addChild(leftAiLabel);
 
-
-		//creat first spot next to player based off his index.
+		//all the players across the top spot.
 		if(this._isFivePlayer){
-			this.createAiTopSpots(2, aiIndex);
+			this.createAiTopSpots(2);
 		}else{
-			this.createAiTopSpots(3, aiIndex);
+			this.createAiTopSpots(3);
 		}
-		var rightOfSpot = cc.Sprite.create(aiSpotMarker);
-		rightOfSpot.setPosition(new cc.Point(winSize.width, y));	
-		this.addChild(rightOfSpot);	
 
-		var rightLabel = cc.LabelTTF.create("Lex Luther", "Arial", 14);
-		rightLabel.setColor(cc.c3b(0,0,0));
-        rightLabel.setPosition(new cc.Point(winSize.width -40, y));
-        this.addChild(rightLabel);
-		//create last spot
+		//ai on the right side and his marker;
+		var rightSpot = createPlayerMarker(winSize.width, y);	
+		this.addChild(rightSpot);	
+		var rightAiLabel = creatAiLabel("Lex Luther", winSize.width - 40, y);
+        this.addChild(rightAiLabel);
 	},
 	displayDealerChip: function () {
-		var dealerChip = cc.Sprite.create(dealerChipRes);
-		var markerX = winSize.width/2 + 50;
-		var markerY = 100;
-		switch(this.CurrentDealerIndex)
-		{
-		case 0:
-		   //0 is always human, even though that might be bad for adding mutliplayer (makes it easyier for me to program so fuckit?)
-			markerX = (winSize.width/2) + 125;
-			markerY = 40;
-	  		break;
-  		case 1:
-  			markerX = 40;
-			markerY = (winSize.height/1.5) - 125;
-  			break;
-		case 2:
-			markerX = winSize.width/3;
-			markerY = winSize.height - 40;
-			break;
-		case 3:
-			markerX = (winSize.width/3)*2;
-			markerY = winSize.height - 40;
-			break;
-		case 4:
-			markerX = winSize.width - 40;
-			markerY = (winSize.height/1.5) - 125;
-			break;
-		default: 
-			//do nothing
-			cc.log('captain, we have a serious problem...somehow the dealer managed to go unassigned. func: displayDealerChip of BidClass.js');
-			break;
-		}
-		dealerChip.setPosition(new cc.Point(markerX, markerY));
+		var dealerChip = createDealerChip(this.CurrentDealerIndex);
 		this.addChild(dealerChip);
 	},
 	displayCurrentPlayersTurnIndicator: function () {
-		var currentTurnIndicator = cc.Sprite.create(currentTurnImg);
-		var markerX = 0;
-		var markerY = 0;
+		var currentTurnIndicator = displayCurrentTurn(this.CurrentPlayersTurnIndex)
 		this.removeChild(this.IndicatorToRemove); //remove the old indicator before adding a new idicator
-		switch(this.CurrentPlayersTurnIndex){
-			case 0: //human is up
-				markerX = winSize.width/2;
-				markerY = 0;
-				break;
-			case 1:
-				markerX = 0;
-				markerY = winSize.height/1.5;
-				break;
-			case 2:
-				var spaceBetween = winSize.width/2;
-				var padding = spaceBetween/2;
-				var x = padding;
-				markerX = x;
-				markerY = winSize.height;
-				break;
-			case 3:
-				var spaceBetween = winSize.width/2;
-				var padding = spaceBetween/2;
-				var x = padding + spaceBetween
-				markerX = x
-				markerY = winSize.height;
-				break;
-			case 4:
-				markerX = winSize.width;
-				markerY = winSize.height/1.5;
-				break;
-			default:
-				//do nothing
-				cc.log("problem dude...no current turn logged.");
-				break;
-
-		}
-		currentTurnIndicator.setPosition(new cc.Point(markerX, markerY));
 		this.IndicatorToRemove = currentTurnIndicator;
 		this.addChild(currentTurnIndicator);
 	},
@@ -440,29 +261,6 @@ var BidClass = cc.LayerColor.extend({
 		}
 
 
-	},
-	createAiTopSpots: function (spotsToCreate,lastPlaceAiIndex){
-		var spaceBetween = winSize.width/spotsToCreate;
-		var padding = spaceBetween/2;
-		var y = winSize.height;
-		var x = padding;
-		for(var i = 0; i < spotsToCreate; i++){
-			var msg = "Dr. Doom";
-			if(i === 1){
-				msg = "Lord Business";
-			}else if(i === 2){
-				msg = "Guy Richie";
-			}
-			var aiSpot = cc.Sprite.create(aiSpotMarker);
-			var nameLabel = cc.LabelTTF.create(msg, "Arial", 14);
-			nameLabel.setColor(cc.c3b(0,0,0));
-        	nameLabel.setPosition(new cc.Point(x, y-30));
-        	
-			aiSpot.setPosition(new cc.Point(x, y));
-			this.addChild(aiSpot);
-			this.addChild(nameLabel);
-			x = x + spaceBetween;
-		}
 	},
 	dealCards: function(){
 		//regardless of 5 or 6 man, in both games everyone is dealt 8 hards.
